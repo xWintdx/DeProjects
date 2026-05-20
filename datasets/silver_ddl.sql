bcc981f7ef871b2eb4e068e1c38d67a2b6058e2e
RB <rbaydushin@gmail.com>
2026-05-20 18:56:17 +0300

    DL — Создал структуру таблиц для слоя Silver

---
+++ datasets/silver_ddl.sql
@@ -0,0 +1,67 @@
+-- 1. Создание схем
+CREATE SCHEMA IF NOT EXISTS silver;
+
+-- 2. Очистка (Опционально, для среды разработки)
+DROP TABLE IF EXISTS silver.cust_info CASCADE;
+DROP TABLE IF EXISTS silver.prd_info CASCADE;
+DROP TABLE IF EXISTS silver.sales_details CASCADE;
+DROP TABLE IF EXISTS silver.cust_az12 CASCADE;
+DROP TABLE IF EXISTS silver.cust_az12 CASCADE;
+DROP TABLE IF EXISTS silver.px_cat_g1v2 CASCADE;
+
+-- 3. Создание таблиц
+CREATE TABLE IF NOT EXISTS silver.cust_info (
+    cst_id INTEGER PRIMARY KEY,
+    cst_key VARCHAR(50) UNIQUE,
+    cst_firstname VARCHAR(50),
+    cst_lastname VARCHAR(50),
+    cst_marital_status VARCHAR(50),
+    cst_gndr VARCHAR(50),
+    cst_create_date DATE,
+    dw_create_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS silver.prd_info (
+    prd_id INTEGER PRIMARY KEY,
+    prd_key VARCHAR(50) UNIQUE,
+    prd_nm VARCHAR(50),
+    prd_cost VARCHAR(50),
+    prd_line VARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+    dw_create_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS silver.sales_details (
+    sls_ord_num INTEGER PRIMARY KEY,
+    sls_prd_key VARCHAR(50) UNIQUE,
+    sls_cust_id INTEGER,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
+    sls_sales NUMERIC,
+    sls_quantity INTEGER,
+    sls_price NUMERIC,
+    dw_create_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS silver.cust_az12 (
+    CID VARCHAR(50) PRIMARY KEY,
+    BDATE TIMESTAMP,
+    GEN VARCHAR(50),
+    dw_create_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS silver.loc_a101 (
+    CID VARCHAR(50) PRIMARY KEY,
+    CNTRY VARCHAR(50),
+    dw_create_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS silver.px_cat_g1v2 (
+    ID VARCHAR(50) PRIMARY KEY,
+    CAT VARCHAR(50),
+    SUBCAT VARCHAR(50),
+    MAINTENANCE VARCHAR(50),
+    dw_create_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    );
